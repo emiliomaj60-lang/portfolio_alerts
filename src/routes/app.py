5f8e5e014118e94a5183f674b83ec10b8acdf429
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-05 22:19:43 +0100

    Add files via upload

--- src/routes/app.py
+++ src/routes/app.py
@@ -3,8 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from src.routes.portfolio import portfolio_bp
-    from src.routes.api import api_bp
+    from routes.portfolio import portfolio_bp
+    from routes.api import api_bp
+
+
 
     app.register_blueprint(portfolio_bp)
     app.register_blueprint(api_bp, url_prefix="/api")
