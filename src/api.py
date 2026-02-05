193b09df97ae05c61d4f9eeda9e3b3729f29aee8
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-05 21:08:59 +0100

    Add files via upload

---
+++ src/api.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify
+from data_access import carica_portafoglio_da_csv
+
+api_bp = Blueprint("api", __name__)
+
+@api_bp.route("/portafoglio")
+def api_portafoglio():
+    portafoglio = carica_portafoglio_da_csv("data/portfolio.csv")
+    titoli = portafoglio.lista_titoli()
+
+    return jsonify([
+        {
+            "isin": t.isin,
+            "symbol": t.symbol,
+            "nome": t.nome,
+            "quantita": t.quantita,
+            "prezzo_carico": t.prezzo_carico
+        }
+        for t in titoli
+    ])
