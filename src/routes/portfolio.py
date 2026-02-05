1ab52c3c2c0c3a6e81a82fbe9178d4aec000fc52
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-05 20:50:52 +0100

    Add files via upload

--- src/routes/portfolio.py
+++ src/routes/portfolio.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from data_access import carica_portafoglio_da_csv
-
+from market_api import get_price
 
 portfolio_bp = Blueprint("portfolio", __name__)
 
@@ -8,4 +8,14 @@ portfolio_bp = Blueprint("portfolio", __name__)
 def index():
     portafoglio = carica_portafoglio_da_csv("data/portfolio.csv")
     titoli = portafoglio.lista_titoli()
+
+    # Aggiungiamo il prezzo attuale a ogni titolo
+    for t in titoli:
+        t.prezzo_attuale = get_price(t.symbol)
+
+        if t.prezzo_attuale:
+            t.gain_loss = ((t.prezzo_attuale - t.prezzo_carico) / t.prezzo_carico) * 100
+        else:
+            t.gain_loss = None
+
     return render_template("index.html", titoli=titoli)
