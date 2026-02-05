ae03b6281a4338510253f0167fce9e9adcc72fda
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-05 19:36:20 +0100

    Add files via upload

---
+++ src/portfolio.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+from src.data_access import carica_portafoglio_da_csv
+
+portfolio_bp = Blueprint("portfolio", __name__)
+
+@portfolio_bp.route("/")
+def index():
+    portafoglio = carica_portafoglio_da_csv("data/portfolio.csv")
+    titoli = portafoglio.lista_titoli()
+    return render_template("index.html", titoli=titoli)
