32b929376ae5b5cd9904a1002eb73e3e9d637c7f
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-06 12:13:09 +0100

    Add files via upload

--- src/data_access.py
+++ src/data_access.py
@@ -1,4 +1,17 @@
 import csv
+
+def carica_costi_gestione(path="data/costi_gestione.csv"):
+    with open(path, newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        row = next(reader)
+
+        return {
+            "spese_acquisto": float(row["spese_acquisto"]),
+            "commissioni_acquisto": float(row["commissioni_acquisto"]),
+            "spese_vendita": float(row["spese_vendita"]),
+            "commissioni_vendita": float(row["commissioni_vendita"])
+        }
+
 from models import Titolo, Portafoglio
 
 def carica_portafoglio_da_csv(percorso_file):
