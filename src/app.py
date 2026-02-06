9e3042ba71e188148bbf340bbb45343a86407cc0
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-06 21:14:43 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 # trigger redeploy
 
@@ -8,9 +8,12 @@ def create_app():
     from routes.portfolio import portfolio_bp
     from routes.api import api_bp
 
-
-
     app.register_blueprint(portfolio_bp)
     app.register_blueprint(api_bp, url_prefix="/api")
+
+    # 🔵 SERVE IL MANIFEST DALLA CARTELLA /src
+    @app.route('/manifest.json')
+    def manifest():
+        return send_from_directory(app.root_path, 'manifest.json')
 
     return app
